d7e46f1e2375286b65682762081335be07c6c1f1
Alessandro <silvaalessandro352@gmail.com>
2025-03-06 08:44:11 -0300

    prisma teste

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,23 +1,23 @@
-// import { PrismaClient } from "@prisma/client";
-// console.log(process.env.DATABASE_URL, 'front e back');
-
-// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from "@prisma/client";
 console.log(process.env.DATABASE_URL, 'front e back');
-
-// Tipando explicitamente NODE_ENV
-const nodeEnv = process.env.NODE_ENV as "development" | "production" | "test";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (nodeEnv !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from "@prisma/client";
+// console.log(process.env.DATABASE_URL, 'front e back');
+
+// // Tipando explicitamente NODE_ENV
+// const nodeEnv = process.env.NODE_ENV as "development" | "production" | "test";
+
+// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// if (nodeEnv !== "production") globalForPrisma.prisma = prisma;
 
 
 
