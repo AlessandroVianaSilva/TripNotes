9059978e7ab9ed78f5465676e3e8b139151aed81
Alessandro <silvaalessandro352@gmail.com>
2025-03-06 01:32:51 -0300

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,20 @@
+// import { PrismaClient } from "@prisma/client";
+// console.log(process.env.DATABASE_URL, 'front e back');
+
+// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
 console.log(process.env.DATABASE_URL, 'front e back');
+
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
