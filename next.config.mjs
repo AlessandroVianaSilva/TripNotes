bd21c2a264dc8dbfb66e0200baf54a9c2408209a
Alessandro <silvaalessandro352@gmail.com>
2025-03-06 00:09:39 -0300

    dotenv

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+// import { dot } from "node:test/reporters";
+
 /** @type {import('next').NextConfig} */
+
+dotenv.config()
+
+
 const nextConfig = {
     images: {
       domains: ['firebasestorage.googleapis.com'],
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
-dotenv.config();
 console.log(process.env.DATABASE_URL, 'front e back');
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
