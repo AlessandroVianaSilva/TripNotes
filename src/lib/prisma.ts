5151bf680cd0a269ac2e8101af64e9631644f1a3
Alessandro <silvaalessandro352@gmail.com>
2025-03-06 01:40:52 -0300

    teste prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,11 +10,14 @@
 import { PrismaClient } from "@prisma/client";
 console.log(process.env.DATABASE_URL, 'front e back');
 
+// Tipando explicitamente NODE_ENV
+const nodeEnv = process.env.NODE_ENV as "development" | "production" | "test";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (nodeEnv !== "production") globalForPrisma.prisma = prisma;
+
+
+
