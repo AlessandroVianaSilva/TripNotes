9dc414ac5476fa5c77095244a2f46e5dc82489e4
Alessandro <silvaalessandro352@gmail.com>
2025-03-06 00:17:42 -0300

    prisma

--- src/components/form/SignUpForm.tsx
+++ src/components/form/SignUpForm.tsx
@@ -47,7 +47,7 @@ const SignUpForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof FormSchema>) => {
-    console.log(process.env.DATABASE_URL, 'front e back')
+    console.log(process.env.NEXT_DATABASE_URL, 'front e back')
     console.log(process.env.NEXT_PUBLIC_SUPABASE_URL, 'front e back')
     const response = await fetch('/api/user', {
       method: "POST",
